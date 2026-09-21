af0df5f1f6ce13c1b17adca833399eb8cc2a8100
Vietze123 <132370543+Vietze123@users.noreply.github.com>
2026-09-21 23:32:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='maeuse-filmliste-v52';
+const CACHE='maeuse-filmliste-v55';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png','./movies.json','./cinematic-floral-bg.jpg',
-  './cinematic-floral-bg-v52.jpg','./suggestions.json'];
+  './cinematic-floral-bg-v53.jpg','./suggestions.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
